9c1f124b37c3ba0919f1ae358a07d98d855db14b
Jorge Alvarado <jlaf1986@yahoo.com>
2012-11-15 07:42:14 +0100

    initial commit

---
+++ SignalRDemo/MyCustomConnection.cs
@@ -0,0 +1,20 @@
+﻿using SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SignalRDemo
+{
+    public class MyCustomConnection : PersistentConnection
+    {
+         
+            protected override Task OnReceivedAsync(IRequest request, string connectionId, string data)
+            {
+                //  return base.OnReceivedAsync(request, connectionId, data);
+                return Connection.Broadcast(connectionId + ":" + data);
+            }
+     
+    }
+}
